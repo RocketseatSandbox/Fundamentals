4426637c9aa961d07c877dd2ea7404f0403334ce
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2022-08-16 00:22:57 -0300

    kindsOfData.js created and lineVersusBlockComments.js done

---
+++ STELAR_GUIDE_OF_JAVASCRIPT/scripts/kindsOfData.js
@@ -0,0 +1,17 @@
+/* 
+    Examples of string using: "", '', ``
+*/
+console.log("Mônica Caroline Carvalho aka 'TheMonicaDev'")
+console.log('""__o_o__""');
+console.log('Guilherme Silva Borges aka "GuilhermeBorgsDdev"')
+console.log(`
+    =====>>
+     O   
+    -0- ${'S' + 2}
+    _|_
+    <<=====
+`)
+
+/* 
+    Examples using type Number:
+*/
---
+++ STELAR_GUIDE_OF_JAVASCRIPT/scripts/lineVersusBlockComments.js
@@ -0,0 +1,8 @@
+console.log("Welcome to the stelar guide of JS!!")
+// Comment inline: console.log("Not show")
+/* 
+    Im a
+    block command,
+    im multline!
+*/
+alert("Welcome to my page!")
