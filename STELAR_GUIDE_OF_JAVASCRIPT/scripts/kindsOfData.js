23703cb2d198ec6646cde7e2f94cfb6063677243
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2022-08-16 23:59:19 -0300

    Booleans and numbers are made on kindsOfData.js

--- STELAR_GUIDE_OF_JAVASCRIPT/scripts/kindsOfData.js
+++ STELAR_GUIDE_OF_JAVASCRIPT/scripts/kindsOfData.js
@@ -11,7 +11,28 @@ console.log(`
     _|_
     <<=====
 `)
-
+console.log(`
+    \n
+    =============================
+    \n
+`)
 /* 
-    Examples using type Number:
+    Examples using type Number: 22, 12.5, NaN, Infinity, expressions and some Math methods
 */
+console.log("All of these are Numbers: ", 22, -22, 25/2, 3/'a', '3'/'a', 100/0)
+console.log('All these expressions returns a type Number too: ', 
+(3 * 3), (2 - 1/2), (3**4), Math.pow(3, 4), Math.sqrt(25), Math.cbrt(27))
+console.log(`
+    \n
+    =============================
+    \n
+`)
+/* 
+    Examples of booleans:
+*/
+console.log("Boolean expressions: ", 1 == "1", 1 === "1", 1 !== '1', 1 != '1', -1 > 0, 0 >= -1)
+console.log(`
+    \n
+    =============================
+    \n
+`)
