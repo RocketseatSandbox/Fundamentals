7ac9d47e6e5dfff909878866a294a905521ac460
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2022-10-18 21:00:55 -0300

    Truphy, false and precedence of operators

--- STELAR_GUIDE_OF_JAVASCRIPT/scripts/EXPRESSIONS_AND_OPERATORS/falsyTruthyAndOperatorsPrecedence.js
+++ STELAR_GUIDE_OF_JAVASCRIPT/scripts/EXPRESSIONS_AND_OPERATORS/falsyTruthyAndOperatorsPrecedence.js
@@ -1,4 +1,46 @@
 /* 
+    Knowing the falsy values
 */
+console.log(
+    !!false,
+    !!0,
+    !!-0,
+    !!"",
+    !!null,
+    !!NaN,
+    !!undefined
+)
+console.log('======================')
 
+/* 
+    Examples of truthy values
+*/
+var symbol = Symbol("ee")
+console.log(
+    !!function(){},
+    !!-1,
+    !!{},
+    !!true,
+    !!" ",
+    !!symbol,
+    !!Infinity
+)
+console.log('======================')
+
+/* 
+    Precedence of operators: 
+      1º grouping = ()
+      2º negation and increments = ! ++ --
+      3º multiplication and division = * / **
+      4º addition and subtraction + -
+      5º relational = < <= > >=
+      6º equity = == != === !==
+      7º AND = &&
+      8º OR  = ||
+      9º conditional = ?:
+      10º assignment = (= += -= *=)
+*/
+let anyValue = null
+console.log((-0 && true) || (0 || true))
+console.log(3 < 4 ** 2 === 7 || (false ? true : (anyValue = "")) + 2)
 console.log('======================')
