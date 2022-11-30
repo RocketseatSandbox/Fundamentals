59e7a2317f8e3363c5733b3eed8a2bf093032d1d
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2022-11-29 21:27:47 -0300

    switchCase.js file is done

--- STELAR_GUIDE_OF_JAVASCRIPT/scripts/CONDITIONALS_ AND_ FLUX_CONTROL/switchCase.js
+++ STELAR_GUIDE_OF_JAVASCRIPT/scripts/CONDITIONALS_ AND_ FLUX_CONTROL/switchCase.js
@@ -1,5 +1,57 @@
 /**
- *  
-*/
+ *  Switch: has a case for expression and a default value
+ *  - The conditions are separated by break instruction
+ */
 
-console.log('======================')
+let message = ""
+const LETTER = 'e'
+
+switch (LETTER) {
+  case "a":
+    message = "First vocal of the alphabet";
+  break;
+  case "e":
+    message = "Second vocal of the alphabet";
+  break;
+  case "i":
+    message = "Third vocal of the alphabet";
+  break;
+  case "o":
+    message = "Fourth vocal of the alphabet";
+  break;
+  case "u":
+    message = "Fifth vocal of the alphabet";
+  break;
+  default:
+    message = "The letter is not a vocal letter"
+}
+
+console.log(message)
+
+console.log("======================");
+
+function calculate(number1, operator, number2) {
+    let result = 0;
+    switch(operator) {
+        case '+':
+            result = number1 + number2
+        break;
+        case '-':
+            result = number1 - number2
+        break;
+        case '*':
+            result = number1 * number2
+        break;
+        case '/':
+            result = number1 / number2
+        break;
+        default: 
+            console.log("Operator is not a valid one")
+        break;
+    }
+    return result
+}
+
+console.log(calculate(1, '/', 2))
+
+console.log("======================");
